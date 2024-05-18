c06409e0d967bf4d33533f203074e6636a81bd4f
Syahrul-123 <105841100721@student.unismuh.ac.id>
2024-05-17 21:46:07 -0700

    tugas 1

--- App.js
+++ App.js
@@ -8,13 +8,13 @@ export default function App() {
   return (
       <View style={{flex: 1, }}>
             <View style={{flex: 1,flexDirection: 'row'}}>
-              <View style={{flex: 1,backgroundColor: 'red',}}
+              <View style={{flex: 1,backgroundColor: 'yellow',}}
               ></View>
               <View style={{flex: 1,backgroundColor: 'green',}}
               ></View></View>
-        <View style={{flex: 1,backgroundColor: 'blue',}}
+        <View style={{flex: 1,backgroundColor: 'red',}}
         ></View>
-        <View style={{flex: 1,backgroundColor: 'purple',}}
+        <View style={{flex: 1,backgroundColor: 'blue',}}
         ></View></View>
   );
 }
