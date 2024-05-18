3d33bddeb5413de7419dc1332c93962da3a298b9
Syahrul-123 <105841100721@student.unismuh.ac.id>
2024-05-17 21:59:44 -0700

    tugas 2

--- App.js
+++ App.js
@@ -6,15 +6,22 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-      <View style={{flex: 1, }}>
-            <View style={{flex: 1,flexDirection: 'row'}}>
-              <View style={{flex: 1,backgroundColor: 'yellow',}}
-              ></View>
-              <View style={{flex: 1,backgroundColor: 'green',}}
-              ></View></View>
-        <View style={{flex: 1,backgroundColor: 'red',}}
-        ></View>
-        <View style={{flex: 1,backgroundColor: 'blue',}}
-        ></View></View>
+    <View style={{flex: 1,}}>
+    <View style={{flex: 1, flexDirection: 'row' }}>
+    <View style={{flex: 1,flexDirection: 'column'}}>
+    <View style={{flex: 1,backgroundColor: 'yellow',}}
+    ></View>
+    <View style={{flex: 1,backgroundColor: 'orange',}}
+    ></View></View>
+   <View style={{flex: 1,flexDirection: 'column'}}>
+    <View style={{flex: 1,flexDirection: 'row'}}>
+
+    <View style={{flex: 1,backgroundColor: 'red',}}
+    ></View>
+    <View style={{flex: 1,backgroundColor: 'purple',}}
+    ></View></View>
+   </View></View>
+      <View style={{flex: 1,backgroundColor: 'blue',}}
+      ></View></View>
   );
 }
