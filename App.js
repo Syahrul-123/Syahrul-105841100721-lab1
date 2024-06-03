05e38dd12ec42ea4b834e9d08a7c364282afbbc8
Syahrul-123 <105841100721@student.unismuh.ac.id>
2024-06-02 23:48:24 -0700

    useFont

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
-import Login from './Login';
-import SignUp from './SignUp';
-import ForgotPassword from './ForgotPasword';
 
-export default function app() {
-  return (
-    <>
-    {<Login/>}
-    </>
+import { useFonts } from 'expo-font'
+const App = () => {
   
-  );
-}
+  const [fontsLoaded, fontError] = useFonts({
+    'Metro-Bold': require('./assets/font/Metropolis-Bold.otf'),
+    'Metro-Medium': require('./assets/font/Metropolis-Medium.otf'),
+    'Metro-Semibold': require('./assets/font/Metropolis-SemiBold.otf'),
+    'Metro-Black': require('./assets/font/Metropolis-Black.otf'),
 
+
+  });
+      if (!fontsLoaded) return 
+      <View>
+          <Text>Font tidak ditemukan !</Text>
+      </View>
+
+  return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Text style={{ fontSize: 30}}>Font Biasa</Text>
+        <Text style={{ fontFamily: 'Metro-Bold', fontSize: 30 }}>Metro Bold</Text>
+        <Text style={{ fontFamily: 'Metro-Medium', fontSize: 30 }}>Metro Medium</Text>
+        <Text style={{ fontFamily: 'Metro-Semibold', fontSize: 30 }}>Metro SemiBold</Text>
+        <Text style={{ fontFamily: 'Metro-Black', fontSize: 30 }}>Metro Black</Text> 
+
+      </View>
+    )
+}
+export default App
