bbe8f20b420b53da5ceccfb342fc4d01f11f0635
Syahrul-123 <105841100721@student.unismuh.ac.id>
2024-07-05 23:47:32 -0700

    UseHook Data & On Change ke 9

--- App.js
+++ App.js
@@ -1,94 +1,44 @@
-import * as React from 'react';
-import { View, Text, Button, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginPage from './pages/LoginPages';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Homeaktif from './assets/Home-aktif.jpg';
-import Homenonaktif from './assets/Home-non-aktif.jpg';
-import Shopaktif from './assets/Shop-aktif.jpg'
-import Shopnonaktif from './assets/Shop-non-aktif.jpg'
-import Shop from './Shop'
-import Home from './Home'
-import Profil from './Profil'
-import { AntDesign} from '@expo/vector-icons'
-import { SimpleLineIcons } from '@expo/vector-icons';
-import Bag from './Bag'
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+const App = () => {
+  const [formLogin, setForm] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+  const onSubmit = () => {
+    if (formLogin.email === 'unismuh'
+      && formLogin.password === 'unismuh') {
+      alert('Login Berhasil')
+    } else {
+      alert('Login Gagal')
+    }
+  }
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={focused ? Homeaktif : Homenonaktif}
-              style={{ width: 30, height: 30 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Shop"
-        component={Shop}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={focused ? Shopaktif : Shopnonaktif}
-              style={{ width: 30, height: 30 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profil"
-        component={Profil}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <AntDesign name='user' color={focused? "red":"gray" } size={30}/>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Bag"
-        component={Bag}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <SimpleLineIcons name='handbag' color={focused? "red":"gray" } size={30}/>
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Form Login</Text>
+      <View>
+        <Text>Email</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(hasil) => setForm({ ...formLogin, email: hasil })}
+          value={formLogin.email}
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => setForm({ ...formLogin, password: text })}
+          value={formLogin.password}
+        />
+        <View style={{ marginTop: 10 }}>
+          <Button title="Login" onPress={onSubmit} />
+        </View>
+        <View>
+          <Text>Email: {formLogin.email}</Text>
+          <Text>Password: {formLogin.password}</Text>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
-
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Main" component={MyTabs} options={{headerShown:false}}/>
-        <Stack.Screen name="Login" component={LoginPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+export default App
